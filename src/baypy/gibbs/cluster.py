1fc2c76e281e31fcff604a77a505603ce4d85132
Paweł Czyż <pczyz@protonmail.com>
2023-03-24 22:15:13 +0100

    Add sampling probability of binary variables given clusters.

--- src/baypy/gibbs/cluster.py
+++ src/baypy/gibbs/cluster.py
@@ -1,7 +1,9 @@
 """Sampling cluster labels and proportions."""
-from jaxtyping import Array, Float, Int
+import jax
 import jax.numpy as jnp
+
 from jax import random
+from jaxtyping import Array, Float, Int
 
 
 def bernoulli_loglikelihood(
@@ -32,8 +34,8 @@ def bernoulli_loglikelihood(
 
 
 def sample_cluster_labels(
-    *,
     key: random.PRNGKeyArray,
+    *,
     cluster_proportions: Float[Array, " B"],
     prob_code_given_cluster: Float[Array, "K B"],
     binary_codes: Int[Array, "N K"],
@@ -65,3 +67,72 @@ def sample_cluster_labels(
 
     # Finally, we can sample the label for each data point
     return random.categorical(key, logits, axis=1)
+
+
+def calculate_counts(labels: Int[Array, " N"], n_clusters: int) -> Int[Array, " B"]:
+    """Calculates the occurrences of each cluster label.
+
+    Args:
+        labels: vector of labels. Each from the set ``{0, 1, ..., B-1}``
+        n_clusters: number of clusters, also denoted as ``B``
+
+    Returns:
+        occurences of eac
+
+    Note:
+        This uses ``jax.numpy.bincount``, so any negative values present
+          (or too large) may be truncated. No checking on our side is used.
+    """
+    return jnp.bincount(labels, length=n_clusters)
+
+
+def sample_cluster_proportions(
+    key: random.PRNGKeyArray,
+    *,
+    labels: Int[Array, " N"],
+    dirichlet_prior: Float[Array, " B"],
+) -> Float[Array, " B"]:
+    """Samples the cluster proportions.
+
+    Args:
+        key: JAX random key
+        labels: cluster labels, from the set :math:`\\{0, 1, ..., B-1\\}`,
+          for each data point
+        dirichlet_prior: parameters :math`\\alpha` of the Dirichlet
+          distribution specifying the prior
+
+    Returns:
+        sampled cluster proportions: entries between (0, 1), summing up to 1
+    """
+    n_clusters = dirichlet_prior.shape[0]
+    counts = calculate_counts(labels=labels, n_clusters=n_clusters)
+    return random.dirichlet(key, alpha=counts + dirichlet_prior)
+
+
+def sample_prob_code_given_cluster(
+    key: random.PRNGKeyArray,
+    *,
+    codes: Int[Array, "N K"],
+    labels: Int[Array, " N"],
+    n_labels: int,
+    prior_a: float = 1.0,
+    prior_b: float = 1.0,
+) -> Float[Array, "K B"]:
+    """Samples the probability matrix
+      P(code[k]=1 | cluster = b)
+    given the observations.
+
+    Todo:
+        Refactor this function into deterministic summary statistic
+          calculation and then sampling.
+    """
+    labels_encoded = jax.nn.one_hot(labels, n_labels)
+
+    statistic_success: Int[Array, "N B"] = jnp.einsum(
+        "NK,NB->KB", codes, labels_encoded
+    )
+    statistic_fails: Int[Array, "N B"] = jnp.einsum(
+        "NK,NB->KB", 1 - codes, labels_encoded
+    )
+
+    return random.beta(key, prior_a + statistic_success, prior_b + statistic_fails)
