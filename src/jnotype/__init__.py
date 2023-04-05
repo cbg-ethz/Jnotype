c02558adcfc367e1359d45ac873acb1d360beef5
Paweł Czyż <pczyz@protonmail.com>
2023-04-05 11:13:45 +0200

    Add block images sampler.

--- src/jnotype/__init__.py
+++ src/jnotype/__init__.py
@@ -1,6 +1,8 @@
 """Exploratory analysis of binary data."""
 import jnotype.bmm as bmm
+import jnotype.datasets as datasets
 
 __all__ = [
     "bmm",
+    "datasets",
 ]
---
+++ src/jnotype/datasets/_simulation/__init__.py
@@ -0,0 +1,6 @@
+"""Simulated data sets."""
+from jnotype.datasets._simulation._block_images import BlockImagesSampler
+
+__all__ = [
+    "BlockImagesSampler",
+]
