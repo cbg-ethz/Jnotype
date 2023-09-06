7074e36b285bde665d1677020e0586eade235d6e
Paweł Czyż <pczyz@protonmail.com>
2023-09-06 21:42:04 +0200

    Add to public API

--- src/jnotype/pyramids/__init__.py
+++ src/jnotype/pyramids/__init__.py
@@ -1,7 +1,9 @@
 """Gibbs samplers for Bayesian pyramids."""
 
 from jnotype.pyramids._sampler_fixed import TwoLayerPyramidSampler
+from jnotype.pyramids._sampler_csp import TwoLayerPyramidSamplerNonparametric
 
 __all__ = [
     "TwoLayerPyramidSampler",
+    "TwoLayerPyramidSamplerNonparametric",
 ]
