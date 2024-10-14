03d9babc151c981386869f490024e2a276607146
Paweł Czyż <pczyz@protonmail.com>
2024-10-14 04:00:53 +0200

    Add full models and posterior predictive checks

--- src/jnotype/checks/__init__.py
+++ src/jnotype/checks/__init__.py
@@ -1,5 +1,16 @@
 """Subpackage with generic predictive checks."""
 
 from jnotype.checks._histograms import plot_histograms, calculate_quantiles
+from jnotype.checks._statistics import (
+    calculate_mcc,
+    calculate_mutation_frequencies,
+    calculate_number_of_mutations_histogram,
+)
 
-__all__ = ["plot_histograms", "calculate_quantiles"]
+__all__ = [
+    "plot_histograms",
+    "calculate_quantiles",
+    "calculate_mcc",
+    "calculate_mutation_frequencies",
+    "calculate_number_of_mutations_histogram",
+]
