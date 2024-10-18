996e578c159d5b01330273bbfc2b3f3bacd84376
Paweł Czyż <pczyz@protonmail.com>
2024-10-18 18:16:08 +0200

    Add functions converting genotypes to the codes.

--- src/jnotype/checks/_statistics.py
+++ src/jnotype/checks/_statistics.py
@@ -1,6 +1,7 @@
 """Convenient summary statistics,
 useful e.g. for posterior predictive checking."""
 
+import jax
 import jax.numpy as jnp
 from jaxtyping import Array, Int, Float
 
@@ -17,7 +18,7 @@ def calculate_mutation_frequencies(X: _DataSet) -> Float[Array, " n_genes"]:
     return jnp.mean(X, axis=0)
 
 
-def calculate_number_of_mutations_histogram(X: _DataSet) -> Float[Array, " n_genes+1"]:
+def calculate_number_of_mutations_histogram(X: _DataSet) -> Int[Array, " n_genes+1"]:
     """Creates an array counting the samples with a specific
     number of mutations.
 
@@ -45,3 +46,55 @@ def calculate_mcc(X: _DataSet) -> Float[Array, "n_genes n_genes"]:
            (i.e., the mutation frequency is 0 or 1)
     """
     return jnp.corrcoef(X, rowvar=False)
+
+
+def _convert_binary_code_to_integer(x: Int[Array, " n_genes"]) -> Int[Array, " "]:
+    """A binary genotype represented as an array
+    is converted into the integer with corresponding
+    binary representation.
+    """
+    n_genes = x.shape[0]
+    bit_positions = jnp.arange(n_genes - 1, -1, -1)
+    powers = jnp.power(2, bit_positions)
+    return jnp.sum(powers * x)
+
+
+def convert_genotypes_to_integers(X: _DataSet) -> Int[Array, " n_samples"]:
+    """Each binary genotype is converted into the
+    integer with corresponding representation in
+    the binary numeral system.
+    """
+    return jax.vmap(_convert_binary_code_to_integer)(X)
+
+
+def convert_integers_to_genotypes(
+    integers: Int[Array, " n_samples"], n_genes: int
+) -> _DataSet:
+    """Maps each integer to a binary genotype.
+
+    Args:
+        Y (jnp.ndarray): Array of integers with shape (n_samples,).
+        n_genes (int): Number of genes (bits) in the genotype.
+
+    Returns:
+        jnp.ndarray: Binary genotype array with shape (n_samples, n_genes).
+    """
+    # Create an array of bit positions
+    bit_positions = jnp.arange(n_genes - 1, -1, -1)
+    # Right-shift and mask to get the bits
+    genotypes = (integers[:, None] >> bit_positions) & 1
+    return genotypes
+
+
+def calculate_atoms_occurrence(X: _DataSet) -> Int[Array, " 2**n_genes"]:
+    """For each unique genotype counts the number of matching samples.
+
+    Note:
+        The returned array has exponentially large length, so that this
+        function should be avoided for large gene sets.
+    """
+    indices = convert_genotypes_to_integers(X)
+
+    n_genes = X.shape[1]
+    length = jnp.power(2, n_genes)
+    return jnp.bincount(indices, length=length)  # type: ignore
