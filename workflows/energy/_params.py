d8780eb8b65e97f1c0b79054c642936ac0af9eab
allenxzhao <allenxzhao@outlook.com>
2025-03-31 20:28:34 +0200

    update params

--- workflows/energy/_params.py
+++ workflows/energy/_params.py
@@ -8,3 +8,6 @@ SPIKE_WEIGHT = 0.1  # probability that off-diagonal entries are non-zero
 SPIKE_SCALE = 10.0  # scale for the off-diagonal spike normal distribution
 DIAG_SCALE = 5.0  # scale for the diagonal normal distribution
 SLAB_PRIOR = 0.01  # scale for the slab prior in Bayes
+NUM_WARMUP = 200  # number of warmup iterations for MCMC
+NUM_SAMPLES = 1000  # number of samples for MCMC
+NUM_CHAINS = 4
