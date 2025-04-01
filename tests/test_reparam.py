5006bd1a512100e2c5976b0f60b123251163cbc7
Paweł Czyż <pczyz@protonmail.com>
2025-04-01 21:37:42 +0200

    Add tests for empirical distribution

--- tests/test_reparam.py
+++ tests/test_reparam.py
@@ -52,3 +52,54 @@ def test_array_conversions(shape, key=42):
             f(b),
             re.func_to_01(f)(a),
         )
+
+
+@pytest.mark.parametrize("input_list", [True, False])
+def test_empirical_distribution_example(input_list: bool):
+    vectors = [
+        # ---
+        jnp.asarray([1, 1, 0]),
+        jnp.asarray([1, 1, 0]),
+        # ---
+        jnp.asarray([0, 1, 0]),
+        jnp.asarray([0, 1, 0]),
+        # ---
+        jnp.asarray([1, 1, 1]),
+        # ---
+        jnp.asarray([0, 0, 0]),
+    ]
+    if not input_list:
+        vectors = jnp.asarray(vectors)
+
+    dist = re.empirical_binary_vector_distribution(vectors)
+    assert dist.n_datapoints == 6
+    assert dist.n_atoms == 4
+    npt.assert_array_equal(dist.counts, jnp.asarray([2, 2, 1, 1]))
+    npt.assert_allclose(dist.weights, jnp.asarray([2 / 6, 2 / 6, 1 / 6, 1 / 6]))
+
+    atoms = jnp.asarray(
+        [
+            [1, 1, 0],
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0],
+        ]
+    )
+
+    npt.assert_array_equal(dist.atoms, atoms)
+
+
+@pytest.mark.parametrize("shape", [(15, 1), (10, 2), (30, 2)])
+@pytest.mark.parametrize("function_desc", FUNCS.items())
+def test_summaries_empirical_distribution(shape, function_desc, key=42):
+    key = jax.random.PRNGKey(key)
+    data = jax.random.bernoulli(key, p=0.3, shape=shape)
+
+    dist = re.empirical_binary_vector_distribution(data)
+
+    name, function = function_desc
+    print(name)
+    f_out = jax.vmap(function)(data)
+
+    assert pytest.approx(dist.calculate_function_mean(function)) == jnp.mean(f_out)
+    assert pytest.approx(dist.calculate_function_sum(function)) == jnp.sum(f_out)
